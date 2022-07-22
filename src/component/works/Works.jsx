ab3e58e8322f0687b0d4650c800291dd9b541f2c
Matielota <matias_vll@hotmail.com>
2022-07-22 12:05:57 -0300

    asfsafsa

--- src/component/works/Works.jsx
+++ src/component/works/Works.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { greatView } from "../../responsive.js";
 import {  useSelector} from "react-redux";
 import { Slide } from "react-awesome-reveal";
+import { Height } from '@material-ui/icons';
 
 export default function Works() {
 const color= useSelector((state)=>state.color)
@@ -114,6 +115,12 @@ const MagicWallCectionul=styled.ul`
   display: -webkit-box;
   flex-wrap: wrap;
 `  
+
+const ProjectContainer=styled.div`
+  background-color:white;
+  height:450px;
+  width:350px
+`
   return (
     <WorkContainer id="WorkContainer">
       <SectionWork id="WorkContainer2">
@@ -163,15 +170,51 @@ const MagicWallCectionul=styled.ul`
       <MagicWallSection id="WorkContainer26">
           <MagicWallCectionul id="WorkContainer27">
             
-            <WorkContent id="WorkContainer28" img={"https://ucarecdn.com/c56723ea-8f2e-42c2-965d-25621e0db538/imagenclickcare.jpg"} linkto={"https://deploy-click-care.vercel.app/"}/>
-        
-            <WorkContent id="WorkContainer29" img={"https://is3-ssl.mzstatic.com/image/thumb/Purple122/v4/2c/09/3e/2c093eec-11ec-b433-d631-7ae419ab65ff/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png"} linkto={"https://climapp-matielota.vercel.app/"}/>
             
-            <WorkContent id="WorkContainer30" img={"https://firebasestorage.googleapis.com/v0/b/ecomerce-325cc.appspot.com/o/matielotasshop.jpg?alt=media&token=0436680e-1482-4cb5-8476-60cb6b8c7104"} linkto={"https://deploy-front-ecomerce.vercel.app/"}/>
+            <WorkContent id="WorkContainer28" img={"https://ucarecdn.com/c56723ea-8f2e-42c2-965d-25621e0db538/imagenclickcare.jpg"} 
+            linkto={"https://deploy-click-care.vercel.app/"}
+            linkto2={"https://github.com/Matielota/ClickCareFront"}
+            textTop={`Single Page Aplication, con la finalidad de acercar, profesionales con pacientes que loo necesiten;
+            promoviendo la democratizacion de la salud. Puedes ver el flujo en el deploy`}
+            textTop2={`React.js Redux, CSS3, Styled componentExpress, Node.js, Jwt. PostgreSQL, MySql, mercado pago entre otras tecnologias.`}
+            title={"ClickCare"}
+            />
+        
+            <WorkContent id="WorkContainer29" img={"https://is3-ssl.mzstatic.com/image/thumb/Purple122/v4/2c/09/3e/2c093eec-11ec-b433-d631-7ae419ab65ff/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png"} 
+            linkto={"https://climapp-matielota.vercel.app/"}
+            linkto2={"https://github.com/Matielota/climapp"}
+            textTop={`Single Page Aplication, con la finalidad de proveer informacion sobre el clima, por geolocalizacion detecta tu ciudad
+            pero tambien puedes buscar la que desees.`}
+            textTop2={`React.js, CSS3, HTML, Styled component "openweathermap"`}
+            title={"Climapp"}
+            />
+            
+            <WorkContent id="WorkContainer30" img={"https://firebasestorage.googleapis.com/v0/b/ecomerce-325cc.appspot.com/o/matielotasshop.jpg?alt=media&token=0436680e-1482-4cb5-8476-60cb6b8c7104"} 
+            linkto={"https://deploy-front-ecomerce.vercel.app/"}
+            linkto2={"https://github.com/Matielota/deployFrontEcomerce"}
+            textTop={`Se trata de un ecommerce programado como single page aplication, en donde puedes registrarte para realizar transacciones en este caso
+            ropa, la aplicacion cuenta como metodo de pago stripe, con un panel de administrador para editar y agregar productos.`}
+            textTop2={`React.js Redux, CSS3, Styled component ,Express, Node.js, Jwt. MongoDB stripe`}
+            title={"MatielotaShop"}
+            />
 
-            <WorkContent id="WorkContainer31" img={"https://firebasestorage.googleapis.com/v0/b/ecomerce-325cc.appspot.com/o/1658259168646dog.png?alt=media&token=c961c03f-30dd-45f4-8ccc-8b9ed743da29"} linkto={"https://dogis-front-end.vercel.app/"}/>
+            <WorkContent id="WorkContainer31" img={"https://firebasestorage.googleapis.com/v0/b/ecomerce-325cc.appspot.com/o/1658259168646dog.png?alt=media&token=c961c03f-30dd-45f4-8ccc-8b9ed743da29"} 
+            linkto={"https://dogis-front-end.vercel.app/"}
+            linkto2={"https://github.com/Matielota/dogis-front-end"}
+            textTop={`Mi primera SPA, perdon si el codigo esta desorganizado, es una app con informacion sobre razas de perros, en donde puedes ver nombres
+            y algunas caracteristicas especificas de la misma, tambien puedes crear razas que no esten en la pagina. `}
+            textTop2={`React.js Redux, CSS3,HTML, Node.js, PostgreSQL`}
+            title={"DogiesApp"}
+            />
+            
 
-            <WorkContent id="WorkContainer32" img={"https://firebasestorage.googleapis.com/v0/b/ecomerce-325cc.appspot.com/o/portfoliotapa.jpg?alt=media&token=543edb7e-97ba-4e9a-96b9-e216ba12fe1b"} linkto={"https://portfolio-matielota.vercel.app/"}/>
+            <WorkContent id="WorkContainer32" img={"https://firebasestorage.googleapis.com/v0/b/ecomerce-325cc.appspot.com/o/portfoliotapa.jpg?alt=media&token=543edb7e-97ba-4e9a-96b9-e216ba12fe1b"} 
+            linkto={"https://portfolio-matielota.vercel.app/"}
+            linkto2={"https://github.com/Matielota/Portfolio"}
+            textTop={`Simplemente mi portafolio, lo agrego por si alguien quiere observar el codigo`}
+            textTop2={`React.js Redux, CSS3,HTML`}
+            />
+            
             
           </MagicWallCectionul>
       </MagicWallSection>
--- src/component/worksContent/WorkContent.jsx
+++ src/component/worksContent/WorkContent.jsx
@@ -1,16 +1,114 @@
 import React from 'react'
 import "./workcontent.css"
+import styled,{ keyframes } from "styled-components";
+import { SiVercel,SiGithubactions} from 'react-icons/si';
+import { useSelector } from 'react-redux';
+import { faWindows } from '@fortawesome/free-brands-svg-icons';
 
-export default function WorkContent({img,linkto}) {
+export default function WorkContent({img,linkto,linkto2,textTop,textTop2,title}) {
+  const color= useSelector((state)=>state.color)
+
+
+
+
+const ProjectTitle=styled.h1`
+margin-top:10px;
+height:50px;
+text-align:center;
+justify-content:center;
+items-align:center;
+`
+const DataContainer=styled.div`
+height: 350px;
+object-fit: fill;
+`
+const ButtonsContainer=styled.div`
+height:75px;
+  bottom:15%
+  background-color:gray;
+  font-size:3rem;
+  color: ${color};
+  margin-left:15%;
+  margin-Top:10px;
+`
+const ImgContainer=styled.img`
+border: 1px solid;
+border-radius:50%;
+height:330px;
+width:330px;
+margin-top:10px;
+margin-left:10%;
+`
+const TextTop=styled.div`
+position:absolute;
+font-size:1.4rem;
+top: 100px;
+height: auto;
+width: 330px;
+background-color:${color};
+text-align:center;
+opacity:0;
+border-radius: 10px;
+`
+const TextTop2=styled.div`
+position:absolute;
+bottom: 20px;
+font-size:1.3rem;
+right: 0;
+height: auto;
+width: 230px;
+background-color:${color};
+opacity:0;
+border-radius: 10px;
+`
+
+const AnimationData=keyframes`
+0%{transform:  translateX(-330px)}
+100%{transform: translateX(0px);}
+`
+const AnimationData2=keyframes`
+0%{transform:  translateX(400px)}
+100%{transform: translateX(0px);}
+`
+const LiMagicWall=styled.div`
+  width: 420px;
+  height: 500px;
+  position: relative;
+  display: list-item;
+  overflow: hidden;
+  margin-left:50px;
+  margin-bottom:50px;
+  &:hover ${TextTop}{opacity:1; animation:${AnimationData} 3s}
+  &:hover ${TextTop2}{opacity:1; animation:${AnimationData2} 3s}
+`
+const Links=styled.a`
+text-decoration: none;
+color:${color};
+&:hover{
+  color:#909096;
+}
+`
   return (
     <div>
-      <li className='li-magic-wall'>
-                <div className="magic-wall-items lazyLoad">
-                    <img src={img}  className="image-work-content" alt="image" />
-                    <div className="hover-content"></div>
-                    <a href={linkto} className='colorbox'></a>
-                </div>
-            </li>
+      <LiMagicWall>
+             <TextTop> {textTop}</TextTop>
+             <TextTop2> {textTop2}</TextTop2>
+                <ProjectTitle>{title}</ProjectTitle>
+                    <DataContainer> 
+                    <ImgContainer src={img} alt="" />
+
+
+                    </DataContainer>
+           
+                <ButtonsContainer>
+                  <Links href={linkto}>
+                    <SiVercel style={{zIndex:"35"}}/>
+                  </Links>
+                  <Links href={linkto2}>
+                    <SiGithubactions style={{marginLeft:"20px"}}/>
+                  </Links>
+                </ButtonsContainer>
+      </LiMagicWall>
     </div>
   )
 }
