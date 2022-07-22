baba75b5f493f661e0dbe40c0df4d5e9b414486c
Matielota <matias_vll@hotmail.com>
2022-07-22 12:32:36 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {
-  BrowserRouter,
+
   Routes,
   Route,
 } from "react-router-dom";
@@ -7,7 +7,7 @@ import About from "./component/about/About";
 import Contact from "./component/Contact/Contact";
 import Works from "./component/works/Works";
 import Home from "./pages/home/Home";
-import ClickCare from "./pages/projects/clickCare/ClickCare";
+
 import Setting from "./Setting";
 
 
@@ -20,7 +20,6 @@ const App = () => {
       <Route path="/about" element={<About/>}/>
       <Route path="/contact" element={<Contact/>}/>
       <Route path="/projects" element={<Works/>}/>
-      <Route path="/projects/clickcare" element={<ClickCare/>}/>
       <Route path="/setting" element={<Setting/>}/>
     </Routes>
   );
