9295e2f422896680b66101f5e30812ea3df2d8c8
Matielota <matias_vll@hotmail.com>
2022-08-04 16:34:53 -0300

    Update Works.jsx

--- src/component/works/Works.jsx
+++ src/component/works/Works.jsx
@@ -199,12 +199,14 @@ const ProjectContainer=styled.div`
             linkto2={"https://github.com/Matielota/Portfolio"}
             textTop={`Simplemente mi portafolio, lo agrego por si alguien quiere observar el codigo`}
             textTop2={`React.js Redux, CSS3,HTML`}
+            title={"Portfolio"}
             />
             <WorkContent id="WorkContainer33" img={"https://firebasestorage.googleapis.com/v0/b/ecomerce-325cc.appspot.com/o/devsCrushPortada.png?alt=media&token=8ff3596f-9ce1-480f-a4b3-7784ec5378a6"} 
             linkto={"https://devscrush.vercel.app/game"}
             linkto2=""
             textTop={`Juego busca Parejas de 3 y 4 elementos para sumar puntos `}
             textTop2={`React.js,CSS3,HTML`}
+            title={"DevsCrush"}
             />
             
           </MagicWallCectionul>
